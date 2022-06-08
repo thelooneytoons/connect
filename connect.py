36f56ff3a65325a3d56f4d232c409d03950a4356
thelooneytoons <63376600+thelooneytoons@users.noreply.github.com>
2022-06-08 14:49:26 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -18,7 +18,7 @@ def home():
 @app.route('/home/<int:num>', methods = ['GET'])
 def disp(num):
   mydb = connector.connect(
-  user="sql-instance-6",
+  user="new-user-5",
   database="sql-db-2",
   )
 
