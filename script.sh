270b6b6813bce4da4023c71a7c9bf31ada4cb195
thelooneytoons <63376600+thelooneytoons@users.noreply.github.com>
2022-06-08 12:58:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-pip install -r requirements.txt;
+pip3 install -r requirements.txt;
 curl https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -o cloud_sql_proxy;
 chmod +x cloud_sql_proxy;
 ./cloud_sql_proxy -instances=sql-instance-6=tcp:3306 &
