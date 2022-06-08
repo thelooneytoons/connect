f3169b96a3f5c5824df412cddd95837461fe364e
thelooneytoons <63376600+thelooneytoons@users.noreply.github.com>
2022-06-08 14:24:30 +0530

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
-#! /bin/bash
-
 pip3 install -r requirements.txt;
 curl https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -o cloud_sql_proxy;
 chmod +x cloud_sql_proxy;
-./cloud_sql_proxy -instances=theloonesql-instance-6=tcp:3306 &
-python3 connect.py;
+./cloud_sql_proxy -instances=thelooneytoons-tasks:us-central1:sql-instance-6=tcp:3306 &
+python3.8 connect.py;
