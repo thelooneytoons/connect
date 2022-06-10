2ab2ced6b0e8c1270f3975929ad83a4e38557188
thelooneytoons <63376600+thelooneytoons@users.noreply.github.com>
2022-06-10 17:51:46 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,5 +0,0 @@
-pip3 install -r requirements.txt;
-curl https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -o cloud_sql_proxy;
-chmod +x cloud_sql_proxy;
-./cloud_sql_proxy -instances=thelooneytoons-tasks:us-central1:sql-instance-6=tcp:3306 &
-python3.8 connect.py;
